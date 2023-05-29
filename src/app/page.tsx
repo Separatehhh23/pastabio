7b5d4149b2c6e10dd819caeab9dcf61874cdf31a
Separatehhh23 <separatehhh23@gmail.com>
2023-05-29 17:04:41 +0200

    Modified NavBar, app and removed settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import styles from '../styles/LandingPage.module.css';
-import NavBar from '../components/NavBar/NavBar';
+import styles from '../styles/LandingPage.module.scss';
+import NavBar from '@/components/NavBar/NavBar';
 
 const LandingPage: React.FC = () => {
   return (
     <div className={styles.container}>
       <NavBar />
-      <h1 className={styles.heading}>Welcome to My Next.js Landing Page</h1>
-      <p className={styles.text}>This is a sample landing page created with Next.js and React.</p>
+      <h1 className={styles.heading}>Hola, crees que esto queda vien?</h1>
+      <p className={styles.text}>Estos botones no hacen nada, son de prueba</p>
       <div className={styles.buttonContainer}>
         <button className={styles.button} type="button">Button 1</button>
         <button className={styles.button} type="button">Button 2</button>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,25 +1,45 @@
 import React from 'react';
 import styles from './NavBar.module.scss';
-import Link from 'next/link';
+
+import ArrowIcon from '../../../public/svgs/arrow.svg';
+import BellIcon from '../../../public/svgs/bell.svg';
+import BoltIcon from '../../../public/svgs/bolt.svg';
+import CaretIcon from '../../../public/svgs/caret.svg';
+import ChevronIcon from '../../../public/svgs/chevron.svg';
+import CogIcon from '../../../public/svgs/cog.svg';
+import MessengerIcon from '../../../public/svgs/messenger.svg';
+import PlusIcon from '../../../public/svgs/plus.svg';
+import NextIcon from '../../../public/svgs/next.svg';
+import VercelIcon from '../../../public/svgs/vercel.svg';
 
 const NavBar: React.FC = () => {
     return (
-      <nav className={styles.nav}>
-        <ul className={styles.navList}>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-          <li>
-            <a href="/settings/settings">Settings</a>
-          </li>
-        </ul>
-      </nav>
+      <div className='NavBar'>
+        <nav className={styles.nav}>
+          <ul className={styles.navList}>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+            <li>
+              <a href="/test">Test</a></li>
+          </ul>
+        </nav>
+        <nav className={styles.nav2}>
+          <ul className={styles.navButons}>
+            <li>
+              <button>
+                
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
     );
   };  
 
