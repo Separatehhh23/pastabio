95bbd55422a892eddc762cc280f41142feac54e9
Separatehhh23 <separatehhh23@gmail.com>
2023-06-02 19:50:31 +0200

    Added global css varible for text color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import styles from '../styles/LandingPage.module.scss';
 import NavBar from '../components/NavBar/NavBar';
-import bgBlob from '../components/backgroundBlob/bgBlob';
 import Link from 'next/link';
 
 function LandingPage() {
 
-  function presentacionPressed() {
-    console.log('Button Pressed');
-  }
-
   return (
     <div className={styles.container}>
       <NavBar />
-      <h1 className={styles.heading}>PASTABIO</h1>
+      <h1 className={styles.heading}>
+        <span className={styles.magic}>PASTABIO</span>
+      </h1>
       <p className={styles.text}>Hola</p>
       <div className={styles.buttonContainer}>
-        <Link href="">
+        <Link href="/games/pastabio">
           <button className={styles.button} type="button">
             Juego
           </button>
