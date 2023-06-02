ed5d3107e7ca5aaf3bc4811063fd24a34c12b59b
Separatehhh23 <separatehhh23@gmail.com>
2023-06-02 20:29:44 +0200

    Added utils/loadFonts.js

--- next.config.js
+++ next.config.js
@@ -10,3 +10,20 @@ module.exports = {
     includePaths: [path.join(__dirname, 'styles')],
   },
 };
+
+// next.config.js
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const originalEntry = config.entry;
+      config.entry = async () => {
+        const entries = await originalEntry();
+        if (entries['main.js'] && !entries['main.js'].includes('./src/utils/loadFonts.js')) {
+          entries['main.js'].unshift('./src/utils/loadFonts.js');
+        }
+        return entries;
+      };
+    }
+    return config;
+  },
+};
